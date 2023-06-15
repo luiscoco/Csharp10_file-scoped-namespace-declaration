debe4a6bbdb0ff0001973d8dd0ca3fd0e106374d
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-15 22:32:58 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyProgram
+{
+    // File-scoped namespace declaration
+    namespace Utilities
+    {
+        class Helper
+        {
+            public static void PrintMessage()
+            {
+                Console.WriteLine("Hello, from the Helper class!");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Utilities.Helper.PrintMessage(); // Accessing the helper class method
+        }
+    }
+}
